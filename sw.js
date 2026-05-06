4421fadc3504482e863195cefae44d7d98f537f5
小山堅斗 <issyanomakku@gmail.com>
2026-05-06 18:29:59 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-importScripts('./js/common/pwa/cache-config.js');
+var SW_BUILD_VERSION = '2026-05-06-002';
+importScripts('./js/common/pwa/cache-config.js?v=' + SW_BUILD_VERSION);
 
 var config = self.MESOROGIA_PWA_CACHE_CONFIG;
 var staticCacheName = config.staticCacheName;
@@ -21,6 +22,28 @@ self.addEventListener('install', function (event) {
   );
   self.skipWaiting();
 });
+
+function reloadControlledClients() {
+  return self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function (clientList) {
+    return Promise.all(
+      clientList.map(function (client) {
+        if (!client.url || typeof client.navigate !== 'function') {
+          return Promise.resolve();
+        }
+
+        try {
+          var url = new URL(client.url);
+          url.searchParams.set('sw_refresh', SW_BUILD_VERSION);
+          return client.navigate(url.toString()).catch(function () {
+            return undefined;
+          });
+        } catch (_) {
+          return Promise.resolve();
+        }
+      })
+    );
+  });
+}
 
 self.addEventListener('activate', function (event) {
   if (isLocalDev) {
@@ -53,6 +76,8 @@ self.addEventListener('activate', function (event) {
       );
     }).then(function () {
       return self.clients.claim();
+    }).then(function () {
+      return reloadControlledClients();
     })
   );
 });
@@ -78,11 +103,26 @@ self.addEventListener('fetch', function (event) {
   }
 
   if (event.request.mode === 'navigate') {
-    event.respondWith(networkFirst(event.request, true));
+    event.respondWith(networkOnly(event.request));
     return;
   }
 
   if (requestUrl.origin !== self.location.origin) {
+    return;
+  }
+
+  if (shouldUseNetworkOnly(requestUrl)) {
+    event.respondWith(networkOnly(event.request));
+    return;
+  }
+
+  if (isCardImage(requestUrl)) {
+    event.respondWith(staleWhileRevalidate(event.request));
+    return;
+  }
+
+  if (isCardDataJson(requestUrl)) {
+    event.respondWith(networkFirst(event.request, false));
     return;
   }
 
@@ -105,10 +145,26 @@ function shouldUseCacheFirst(url) {
   });
 }
 
+function shouldUseNetworkOnly(url) {
+  return /\.(?:html|css|js)$/.test(url.pathname);
+}
+
+function isCardDataJson(url) {
+  return /\/public\/(?:cards_latest|cards_latest\.meta|cards_versions|packs|cv|environments)\.json$/.test(url.pathname);
+}
+
+function isCardImage(url) {
+  return /\/img\/(?:\d{5}|00000)\.webp$/.test(url.pathname);
+}
+
 function shouldUseNetworkFirst(url) {
   return config.networkFirstExtensions.some(function (extension) {
     return url.pathname.endsWith(extension);
   });
+}
+
+function networkOnly(request) {
+  return fetch(new Request(request, { cache: 'reload' }));
 }
 
 function cacheFirst(request) {
@@ -120,6 +176,18 @@ function cacheFirst(request) {
     return fetch(request).then(function (networkResponse) {
       return putRuntimeCache(request, networkResponse);
     });
+  });
+}
+
+function staleWhileRevalidate(request) {
+  var fetchPromise = fetch(request).then(function (networkResponse) {
+    return putRuntimeCache(request, networkResponse);
+  }).catch(function () {
+    return undefined;
+  });
+
+  return caches.match(request).then(function (cachedResponse) {
+    return cachedResponse || fetchPromise;
   });
 }
 
